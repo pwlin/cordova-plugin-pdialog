60a76b42d10b8232c046e612bc69ca2e14583fda
pwlin <pwlin05@gmail.com>
2014-09-25 18:52:11 +0200

    Added runOnUiThread

--- src/android/io/github/pwlin/cordova/plugins/pdialog/PDialog.java
+++ src/android/io/github/pwlin/cordova/plugins/pdialog/PDialog.java
@@ -1,19 +1,27 @@
 package io.github.pwlin.cordova.plugins.pdialog;
 
 import java.io.File;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.annotation.TargetApi;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.util.Log;
+
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.PluginResult;
 import org.apache.cordova.CordovaResourceApi;
 
+//@SuppressWarnings("unused")
 public class PDialog extends CordovaPlugin {
+
+	private ProgressDialog pDialogObj;
 
 	/**
 	 * Executes the request and returns a boolean.
@@ -26,38 +34,61 @@ public class PDialog extends CordovaPlugin {
 	 *            The callback context used when calling back into JavaScript.
 	 * @return boolean.
 	 */
-	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-		
-		Log.v("CordovaLog", "execute function");
-		
-		if (action.equals("open")) {
-			Log.v("CordovaLog", "action is open");			
-
-		} else {
-			Log.v("CordovaLog", "action is else");
-			
-		}
+	public boolean execute(final String action, final JSONArray args, CallbackContext callbackContext) throws JSONException {
+		cordova.getActivity().runOnUiThread(new Runnable() {
+			public void run() {
+				if ("init".equals(action)) {
+					try {
+						PDialog.this.init(args);
+					} catch (JSONException e) {
+						//e.printStackTrace();
+					}
+				} else if ("show".equals(action)) {
+					PDialog.this.show();
+				} else if ("dismiss".equals(action)) {
+					PDialog.this.dismiss();
+				} else if ("setProgress".equals(action)) {
+					PDialog.this.setProgress();
+				} else {
+					Log.e("CordovaLog", "PDialog: invalid action: " + action);
+				}
+			}
+		});
 		return true;
-
 	}
 
-	/**
-	 * Identifies if action to be executed returns a value and should be run
-	 * synchronously.
-	 * 
-	 * @param action
-	 *            The action to execute
-	 * @return T=returns value
-	 */
-	public boolean isSynch(String action) {
-		return false;
+	private void init(JSONArray args) throws JSONException {
+		JSONObject argsObj = new JSONObject(args.getString(0));
+		if (argsObj.has("param3")) {
+			Log.v("CordovaLog", "param3 exists.");
+			Log.v("CordovaLog", argsObj.getString("param3"));
+		} else {
+			Log.v("CordovaLog", "param3 does not exists.");
+		}
+		Log.v("CordovaLog", "PDialog: init function");
+		PDialog.this.pDialogObj = new ProgressDialog(cordova.getActivity(), ProgressDialog.THEME_DEVICE_DEFAULT_DARK);
+		//PDialog.this.pDialogObj.setTitle("");
+		PDialog.this.pDialogObj.show();
 	}
 
-	/**
-	 * Called by AccelBroker when listener is to be shut down. Stop listener.
-	 */
-	public void onDestroy() {
+	private void show() {
+		PDialog.this.pDialogObj.show();
 	}
 
+	private void dismiss() {
+		PDialog.this.pDialogObj.dismiss();
+	}
 
+	private void setProgress() {
+
+	}
+	
+	private void setTitle(CharSequence title) {
+		PDialog.this.pDialogObj.setTitle(title);
+	}
+	
+	private void setMessage(CharSequence message) {
+		PDialog.this.pDialogObj.setMessage(message);
+	}
+	
 }
--- www/plugins.PDialog.js
+++ www/plugins.PDialog.js
@@ -7,4 +7,29 @@ PDialog.prototype.init = function(options) {
 	exec(null, null, "PDialog", "init", [options]);
 };
 
+PDialog.prototype.show = function(options) {
+    options = options || {};
+    exec(null, null, "PDialog", "show", [options]);
+};
+
+PDialog.prototype.dismiss = function(options) {
+    options = options || {};
+    exec(null, null, "PDialog", "dismiss", [options]);
+};
+
+PDialog.prototype.setProgress = function(options) {
+    options = options || {};
+    exec(null, null, "PDialog", "setProgress", [options]);
+};
+
+PDialog.prototype.setTitle = function(options) {
+    options = options || {};
+    exec(null, null, "PDialog", "setTitle", [options]);
+};
+
+PDialog.prototype.setMessage = function(options) {
+    options = options || {};
+    exec(null, null, "PDialog", "setMessage", [options]);
+};
+
 module.exports = new PDialog();
